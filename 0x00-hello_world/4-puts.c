8d452023e5a3a599a0844a291966a0e83d8bd7d0
Elijah <eadekoya77@gmail.com>
2022-12-08 20:41:13 -0800

    sec

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- *main - use puts function to print a line
+/**
+ *main -Use puts function to print a line
  *
  *Return: 0
  */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
