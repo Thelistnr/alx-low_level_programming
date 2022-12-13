9d692e94872b763c86ffbd9dcd60ebad96b90300
Elijah <eadekoya77@gmail.com>
2022-12-12 21:24:25 -0800

    fin

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,11 +9,13 @@ int main(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
+	{
 		putchar((n % 10) + '0');
 		if (n == 9)
 			continue;
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 	return (0);
 }
