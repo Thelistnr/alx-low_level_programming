6ce4d65aee065257df7ee50bcabbd4f128cb743e
Elijah <eadekoya77@gmail.com>
2022-12-12 18:47:01 -0800

    pral

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print Alphabet
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letters;
+
+	for (leter = 'a'; letter <= 'z'; letter++)
+		putchar(letter'\n')
+		return (0);
+}
