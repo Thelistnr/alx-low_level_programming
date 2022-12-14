ee08d3b895351dec9a615a9af5eb64538395d790
Elijah <eadekoya77@gmail.com>
2022-12-13 19:45:47 -0800

    prab

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * _abs - Compute absolutes
+ * @int: type for char
+ * Return: n, -n
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
