5101343853bac99414f20d79bf70fb1280c1d74d
Elijah <eadekoya77@gmail.com>
2022-12-13 18:23:23 -0800

    pral

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Print the alphabet in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char l;
+	
+	for (l = a; l <= z; l++)
+	_putchar(l);
+	_putchar('\n');
+	return (0)
+}
