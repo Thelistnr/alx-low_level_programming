a60349849289bafddff3ef8f5b5bbef1df18f6d3
Elijah <eadekoya77@gmail.com>
2022-12-13 19:25:52 -0800

    chn

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_sign -Check for negative, positive, and zero
+ * @n: char to be checked
+ * Return: 0, 1, -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	if (n == 0)
+		_putchar(0);
+		return (0);
+	if (n < 0)
+		_putchar('-');
+		return (-1);
+}
