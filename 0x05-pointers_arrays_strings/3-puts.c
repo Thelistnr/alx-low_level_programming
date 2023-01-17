a7e8a6ea99bd98f471526b6a2c1a10ab648c9725
Elijah <eadekoya77@gmail.com>
2023-01-17 07:15:03 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Print a string followed by new line to std output
+ * @str: Char
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0';
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
