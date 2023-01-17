c8a6aba243a1a5b7a9a01148068101e227f44c58
Elijah <eadekoya77@gmail.com>
2023-01-17 05:39:46 -0800

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 /**
- *
- *
- *
+ *reset_to_98 - update the val of var
+ *@n: int to update
+ *Return: 0
  */
+void reset_to_98(int *n)
+{
+	int n;
 
+	int *p = &n;
+	*p = 98;
+}
