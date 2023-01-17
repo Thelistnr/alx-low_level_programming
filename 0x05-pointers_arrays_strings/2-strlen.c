b9b8e4753cdbdd596c1221b036fefe193a45500a
Elijah <eadekoya77@gmail.com>
2023-01-17 06:56:14 -0800

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - Return the length of string
+ * @s: char
+ * Return: 0;
+ */
+int _strlen(char *s)
+{
+	int a = 0 /* Start counter from 0*/
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
